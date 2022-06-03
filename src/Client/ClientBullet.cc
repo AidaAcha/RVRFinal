b3d8af85227ea3d0579e76e0f5af162f5f8b719c
AidaAcha <48960424+AidaAcha@users.noreply.github.com>
2022-06-03 14:01:49 +0200

    ClientBullet

---
+++ src/Client/ClientBullet.cc
@@ -0,0 +1,31 @@
+#include "ClientBullet.h"
+#include "../Utilities/SDL_App.h"
+#include "../Utilities/GameObject.h"
+#include <SDL_image.h>
+
+ClientBullet::ClientBullet(Vector2 pos_, const char* tex_, int width_, int height_, SDL_App* app_) : 
+GameObject(pos_, tex_, width_, height_, app_)
+{
+
+}
+
+void ClientBullet::render()
+{
+    dstRect->x = pos.x;
+    dstRect->y = pos.y;
+    dstRect->w = width;
+    dstRect->h = height;
+
+    SDL_Point center = {width/2, height - 4};
+    SDL_RenderCopyEx(sdlApp->getRenderer(), tex, currentFrame, dstRect, angle, &center, SDL_FLIP_NONE);
+}
+
+void ClientBullet::setPosition(Vector2 pos_)
+{
+    pos = pos_;
+}
+
+ClientBullet::~ClientBullet()
+{
+
+}
---
+++ src/Client/ClientBullet.h
@@ -0,0 +1,19 @@
+#ifndef ClientBullet_H_
+#define ClientBullet_H_
+#include "../Utilities/GameObject.h"
+
+class Vector2;
+class ClientBullet : public GameObject
+{
+    public:
+    ClientBullet(Vector2 pos_, const char* tex_, int width_, int height_, SDL_App* app_);
+    ~ClientBullet();
+    Type getType() { return type; };
+    void render();
+    void update() {};
+    void lookAtDirection() {};
+    void setPosition(Vector2 pos_);
+
+};
+
+#endif
